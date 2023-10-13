dd9dd0a6fa3b6cac7d0d131ad8b41fb4b459005b
Lopes Filho <loppes@megganet.com.br>
2023-10-13 15:45:11 -0300

    Updat task and copyNonNullProperties created

--- backend/src/main/java/br/com/loppesfh/todolist/task/TaskController.java
+++ backend/src/main/java/br/com/loppesfh/todolist/task/TaskController.java
@@ -1,17 +1,22 @@
 package br.com.loppesfh.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.loppesfh.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -40,5 +45,21 @@ public class TaskController {
         taskModel.setIdUser((UUID) idUser);
         var taskSaved = taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(taskSaved);
+    }
+
+
+    @GetMapping
+    public List<TaskModel> list(HttpServletRequest request) {
+        UUID userId = (UUID) request.getAttribute("userId");        
+        return taskRepository.findByIdUser(userId);
+    }
+
+    @PutMapping("{taskId}")
+    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID taskId, HttpServletRequest request) {
+
+        var task = taskRepository.findById(taskId).orElseThrow(() -> new RuntimeException("Não existe uma task com esse ID"));
+        Utils.copyNonNullProperties(taskModel, task);
+        
+        return taskRepository.save(task);
     }
 }
