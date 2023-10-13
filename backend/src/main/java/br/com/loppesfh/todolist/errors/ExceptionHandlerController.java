18bfc1604ba8ef8b5e6f275e73f65df6ce2f166e
Lopes Filho <loppes@megganet.com.br>
2023-10-13 17:53:26 -0300

    Dockerfile deploy

--- backend/src/main/java/br/com/loppesfh/todolist/errors/ExceptionHandlerController.java
+++ backend/src/main/java/br/com/loppesfh/todolist/errors/ExceptionHandlerController.java
@@ -1,7 +1,6 @@
 package br.com.loppesfh.todolist.errors;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
-    
+
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
 
